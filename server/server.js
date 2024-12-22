1c82092ac02b5e3658b3bbfb22153b51f44309da
Abhay-1718 <abhaysingh80766@gmail.com>
2024-12-22 19:09:23 +0530

    setup routes for signup login and logout

--- server/server.js
+++ server/server.js
@@ -1,24 +1,29 @@
-import express from "express"
-import cors from 'cors';
-import 'dotenv/config';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
 import cookieParser from "cookie-parser";
-import connectDb from './config/mongoDb.js'
+import connectDb from "./config/mongoDb.js";
+import authRouter from './routes/authRoute.js'
 
 const app = express();
-const port = process.env.PORT || 4000
-connectDb()
+const port = process.env.PORT || 4000;
+connectDb();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    credentials:true
-}));
-app.get('/' , (req,res) => {
-res.send("api working ")
-}
-)
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+//api endpoints
+app.get("/", (req, res) => {
+  res.send("api working ");
+});
+app.use('/api/auth', authRouter)
+
 
 app.listen(port, () => {
-    console.log(`server started at PORT : ${port}`);
-    
-})
+  console.log(`server started at PORT : ${port}`);
+});
