b04bfa51c1c76dd0a79170329fc2420e118e24fe
Abhay-1718 <abhaysingh80766@gmail.com>
2024-12-22 19:51:22 +0530

    setup nodemailer for email verification

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
+import transporter from '../config/nodeMailer.js'
 
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -35,6 +36,16 @@ export const register = async (req, res) => {
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 1 * 24 * 60 * 60 * 1000,
     });
+
+   //sending welcome email
+   const mailOptions={
+    from:process.env.SENDER_EMAIL,
+    to: email,
+    subject:'Welcome to somename ',
+    text:`Welcome to somename website. Your account has been created with email id : ${email}`
+   }
+
+   await transporter.sendMail(mailOptions);
 
     return res.json({
       success: true,
